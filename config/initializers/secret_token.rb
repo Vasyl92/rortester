7493c21e096ae89b978a98085f5b8cc94698e0a5
Vasyl Germanskij <rmacciawelli@mail.ru>
2014-04-09 15:07:17 +0300

    Empty project for RoRtester

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rortester::Application.config.secret_token = '79a327289c51ad02af32acbe67ddec695fcf133a78bb1b2548d9eeca69b3b4d6739d2059443d5fc86f8a02077aff8464fd968eefde4b88b8a7e85dc80137694f'
---
+++ spec/requests/main_tests_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+=begin
+describe "MainTests" do
+  describe "GET /main_tests" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get main_tests_index_path
+      response.status.should be(200)
+    end
+  end
+end
+
+=end
+
